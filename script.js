6bb4c0bdd07e396906a62ed5e69feda2016fd6e9
Jeff Titak <jefftitak@Jeffs-MacBook-Air.local>
2024-11-11 22:41:07 -0600

    Website complete

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ function toggleMobileMenu() {
     mobileMenu.style.display = mobileMenu.style.display === "flex" ? "none" : "flex";
   }
 
-  document.getElementById('contactForm').addEventListener('submit', function(e) {
+  document.getElementById('contactForm').addEventListener('submit', handleSubmit);
+
+  function handleSubmit(event) {
+      event.preventDefault(); // Prevents form from actually submitting
+      
+      // Hide the form
+      document.getElementById('contactForm').style.display = 'none';
+      
+      // Show the thank-you message
+      document.getElementById('thank-you-message').style.display = 'block';
+  }
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     emailjs.sendForm('service_92gh8he', 'template_pi93b61', this, 'U6tApi-QiEBFxwAZ2')
